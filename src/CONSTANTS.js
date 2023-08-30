6960f0a5992e375a8a23c0625faea52b41d4a857
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-08-30 14:37:48 +0300

    for deploy

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,10 +1,10 @@
 // CONSTANTS.js
 export const Constants = {
-  firstColor: "#000000", // black
-  secondColor: "#ffffff", // white
-  thirdColor: "#14c6c9" /* "#52B9F3", */,
+  firstColor: "#000000",
+  secondColor: "#ffffff",
+  thirdColor: "#14c6c9",
   LayoutColor: "rgba(139, 213, 255, 0.3)",
-  FooterColor: "rgba(139, 213, 255, 0.3)" /* "rgba(139, 213, 255, 1)" */,
+  FooterColor: "rgba(139, 213, 255, 0.3)",
   menuPadding: "5px",
   menuHeight: "3rem",
   footerHeight: "2rem",
